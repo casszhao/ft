d46448039678a5991be52a9dc4ae12c93a6d6caa
Cass Zhao <cass790412@gmail.com>
2020-08-04 18:46:39 +0100

    add epoch parameters

--- csv_for_ft_new.py
+++ csv_for_ft_new.py
@@ -7,6 +7,8 @@ including:
 import argparse
 import time
 import datetime
+from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+
 
 parser = argparse.ArgumentParser(description='this version use already made txt file')
 # 0
@@ -17,10 +19,10 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('--running', action='store_true', help='running using the original big dataset')
 group.add_argument('--testing', action='store_true', help='testing using the small dataset')
 
-# 2 text col name
-#parser.add_argument('--textcolname', type=str)
-
+# 2 epoch and batch size
 parser.add_argument('--num_train_epochs', '-e', type=int)
+parser.add_argument('--batch_size', '-b', type=int)
+
 # 3
 parser.add_argument('--data', type=str, action='store', choices = ['AG10K', 'wassem', 'tweet50k', 'multi-label'])
 
@@ -35,31 +37,6 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-'''  processing csv to txt, now already had the txt file, no need to process it every time
-if args.testing:
-    data = pd.read_csv(str(args.csvfile)).sample(10)
-elif args.running:
-    data = pd.read_csv(str(args.csvfile))
-
-def basicPreprocess(text):
-    try:
-        processed_text = text.lower()
-        processed_text = re.sub(r'\W +', ' ', processed_text)
-    except Exception as e:
-        print("Exception:", e, ",on text:", text)
-        return None
-    return processed_text
-    
-data[str(args.textcolname)] = data[str(args.textcolname)].apply(basicPreprocess).dropna()
-data = data[str(args.textcolname)]
-data = data.replace('\n', ' ')
-
-
-with open(str(args.csvfile) + '.txt', 'w') as filehandle:
-    for listitem in data:
-        filehandle.write('%s\n' % listitem)
-        '''
-
 if args.LM == 'Bert':
     from transformers import BertTokenizerFast, BertConfig, BertForMaskedLM
     config = BertConfig(vocab_size=28996,
@@ -70,6 +47,7 @@ if args.LM == 'Bert':
                         )
     tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased', do_lower_case=False)
     model = BertForMaskedLM.from_pretrained('bert-base-cased', config=config)
+    #12-layer, 768-hidden, 12-heads, 110M parameters.
 
 elif args.LM == 'RoBerta':
     from transformers import RobertaConfig, RobertaTokenizerFast, RobertaForMaskedLM
@@ -77,11 +55,12 @@ elif args.LM == 'RoBerta':
         vocab_size=50265,
         max_position_embeddings=514,
         num_attention_heads=12,
-        num_hidden_layers=6,
-        type_vocab_size=1,
+        num_hidden_layers=12,
+        type_vocab_size=2,
     )
     tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base', do_lower_case=False)
     model = RobertaForMaskedLM.from_pretrained('roberta-base' ,config=config)
+    # 12-layer, 768-hidden, 12-heads, 125M parameters, roberta-base using the bert-base architecture
 
 elif args.LM == 'XLM':
     from transformers import XLMConfig, XLMTokenizer, XLMWithLMHeadModel
@@ -93,8 +72,10 @@ elif args.LM == 'XLM':
         n_layers = 12,
     )
 
-    tokenizer = XLMTokenizer.from_pretrained('xlm-mlm-enfr-1024', do_lower_case=False)
-    model = XLMWithLMHeadModel.from_pretrained('xlm-mlm-enfr-1024', config=config)
+    tokenizer = XLMTokenizer.from_pretrained('xlm-mlm-xnli15-1024', do_lower_case=False)
+    model = XLMWithLMHeadModel.from_pretrained('xlm-mlm-xnli15-1024', config=config)
+    # 12-layer, 1024-hidden, 8-heads
+    # XLM Model pre-trained with MLM on the 15 XNLI languages.
 
 else:
     print('need to define LM from Bert,RoBerta,XLM')
@@ -112,7 +93,7 @@ def freeze_layer_fun(freeze_layer):
         else:
             pass
 
-print('++++++++++++++++++++++++++++++++++++ freeze layers +++++++++++++++++++++++++++++')
+
 '''
 freeze_layer_1 = '.1.'
 freeze_layer_fun(freeze_layer_1)
@@ -153,20 +134,14 @@ print(f'The model now has {count_parameters(model):,} trainable parameters')
 print('===========================')
 
 
-from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling
-#paths = [str(x) for x in Path(str(args.txtfolder)).glob("**/*.txt")]
+
 file_path = str(args.data) + '_train.csv.txt'
 print('file_path: ', file_path)
 dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path= file_path, block_size=128)
-print('created dataset for LineByLineTextDataset() from folders')
-
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 
-# initial the trainer
 
-from transformers import Trainer, TrainingArguments
-
-dir = str(args.resultpath) + str(args.data) + '_' + str(args.LM) + '_e' + str(args.num_train_epochs)
+dir = str(args.resultpath) + str(args.data) + '_' + str(args.LM) + '_e' + str(args.num_train_epochs) + '_b' + str(args.batch_size)
 
 training_args = TrainingArguments(
     do_train=True,
@@ -174,7 +149,7 @@ training_args = TrainingArguments(
     output_dir=dir,
     overwrite_output_dir=True,
     num_train_epochs= args.num_train_epochs,
-    per_device_train_batch_size=16,
+    per_device_train_batch_size=args.batch_size,
     save_steps=1_000,
     save_total_limit=2,
 )
