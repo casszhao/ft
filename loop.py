bc517a1fafaa2764c3f0c38b2afbba11cd3826e6
Cass Zhao <cass790412@gmail.com>
2020-10-19 16:26:22 +0100

    add a multiclass .py

--- loop.py
+++ loop.py
@@ -480,7 +480,7 @@ print('int(100/increment)+1', int(100/increment)+1)
 loop(train, test, validation, start)
 for i in range(1, int(100/increment)+1):
     percent = start + i*increment
-    if percent <= 10:
+    if percent <= 3:
         print('percent      ', percent)
         loop(train, test, validation, percent)
     else:
