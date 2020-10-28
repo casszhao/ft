51750c4eb488305f8ab738c182341bb19c2111bd
Cass Zhao <cass790412@gmail.com>
2020-10-28 15:24:19 +0000

    add parameter saved name

--- ft_IMDB.py
+++ ft_IMDB.py
@@ -1,15 +1,17 @@
 import torch
 import argparse
 from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+import os
 
 parser = argparse.ArgumentParser(description='fine-tune roberta using IMDB')
 
 parser.add_argument('--epochs', '-e', type=int)
+parser.add_argument('--savedname', type=str, help='the file to save the model')
 
 args = parser.parse_args()
 
-
-
+savedname = args.savedname
+os.makedirs('./' + savedname)
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -22,11 +24,11 @@ else:
 from transformers import RobertaConfig, RobertaTokenizerFast, RobertaForMaskedLM
 
 config = RobertaConfig(vocab_size=50265,
-                           max_position_embeddings=514,
-                           num_attention_heads=12,
-                           num_hidden_layers=12,
-                           type_vocab_size=1,
-                           )
+                       max_position_embeddings=514,
+                       num_attention_heads=12,
+                       num_hidden_layers=12,
+                       type_vocab_size=1,
+                       )
 tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base', do_lower_case=False)
 model = RobertaForMaskedLM.from_pretrained('roberta-base', config=config)
 # 12-layer, 768-hidden, 12-heads, 125M parameters, roberta-base using the bert-base architecture
@@ -35,13 +37,10 @@ dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path='IMDB_train.csv.t
 #dataset = load_dataset("./csv_for_ft_new.py", data_files=file_path)
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
 
-
-dir = 'IMDB_roberta'
-
 training_args = TrainingArguments(
     do_train=True,
     do_predict=True,
-    output_dir=dir,
+    output_dir=savedname,
     overwrite_output_dir=True,
     num_train_epochs= args.epochs,
     per_device_train_batch_size=16,
@@ -59,5 +58,5 @@ trainer = Trainer(
 
 
 trainer.train()
-trainer.save_model(dir)
-print('the language model saved as ', dir)
+trainer.save_model(savedname)
+print('the language model saved as ', savedname)
